13789b2e7cb115d0b135fcfad90f3daf6e2ca8b6
badabingbadaboom <94488439+michaelsoft-wendows@users.noreply.github.com>
2025-02-28 16:16:30 -0500

    data and calculation overhaul
    motion calculation using scipy.odeint implemented, thrust and mass models updated to reflect new data from latest thrust experiment

--- apps/calculation.py
+++ apps/calculation.py
@@ -1,27 +1,59 @@
 from time import sleep
 
 import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+from scipy.integrate import odeint
 
+"""
+All variables are in expressed in SI base units.
+"""
 
-def cal_drag_f(area, drag_mu, velocity, fluid_density=1.225):
-    """Calculates Drag Force"""
-    """The ISA or International Standard Atmosphere states the density of air is 1.225 kg/m3 at sea level and 15 degrees C."""
+def drag(area, drag_mu, velocity, fluid_density  = 1.225):
+    """
+    The ISA or International Standard Atmosphere states the density of air is 1.225 kg/m3 at sea level and 15 degrees C.
+    """
     Df = area*drag_mu*0.5*fluid_density*(velocity**2)
     return Df
 
-def cal_friction_f(total_mass, friction_mu):
-    """Calculates Friction Force"""
+def friction(total_mass, friction_mu):
     Ff = total_mass/1000*9.81*friction_mu
     return Ff
+
+def pressure(cartridge_mass, model = 'van_der_waals'):
+    if model == "ideal":
+        factor = 3.90e9
+        P = factor*cartridge_mass 
+    if model == "van_der_waals":
+        canister_vol = 1.14e-5
+        # CO2 van der waals constants
+        a = 0.364
+        b = 0.00004267
+        v_m = canister_vol*0.04401/cartridge_mass
+        P = (-a/(v_m**2)+2436.1485/(v_m-b))
+    else:
+        raise Exception("Invalid pressure model.") 
+    return P
+
+def thrust(t):
+    """
+    The equation for thrust is based on experiments where the acceleration of a CO2-canister powered cart was measured. 
+    A logistic regression model is applied to velocity data, and the derivative of the model is calculated to produce a thrust curve.
+    """
+    m_test = 0.561718
+    # regression constants
+    a = 2.17606
+    b = 27.883
+    c = -5.03262
+    F_t = m_test*(a*b*np.exp(-b*t-c))/(np.exp(-b*t-c)+1)**2
+    return F_t
 
 def app():
     """
     This is the main app function
     """
     #Constant
-    interval = 0.0017
+    dt = 0.001
 
     ##############################################################################################
     """Get Data"""
@@ -29,170 +61,115 @@ def app():
     form_dva = st.empty()
     with form_dva.container():
         with st.form(key='form_dva'):
-            #Drag
-            # area = st.number_input("Car Front area")
-            # drag_mu = st.number_input("Drag Coeffecient")
 
-            #DVA
-            # car_mass = st.number_input("Car Mass")
+            """
+            default values based on base car for nationals
+                mass = 0.058
+                c_d = 0.7457
+                frontal_area = 2*0.001119287
+
+            """
+
+            frontal_area = st.number_input("Car frontal area (m^2)")
+            drag_coeff = st.number_input("Drag coefficient")
+            car_mass = 0.001*st.number_input("Car mass (g)")
+
+            # for now, friction is zero until an Advanced Options tab is implemented
             # friction_mu = st.number_input("Friction Mu")
-            # submit = st.form_submit_button(label='Submit')
-            area = 0.002
-            drag_mu = 0.03
-            car_mass = 25
-            friction_mu = 0.03
-            submit = True
+            car_mass = 0.058
+            drag_coeff = 0.7457
+            frontal_area = 2*0.001119287
+            friction_mu = 0.
+
+            submit = st.form_submit_button(label='Submit')
 
     try:
         if submit == True:
 
             #Values can not be zero
-            assert(car_mass !=0 and friction_mu !=0), "Can the values be zero 🤔? no."
+            assert(car_mass !=0), "Can the values be zero 🤔? no."
 
             #Clear Form
             form_dva.empty()
 
             with st.spinner('Wait for it...'):
-                """Executing code"""
+                #Executing code
                 sleep(1)
 
-                data = {'time(s)': np.arange(0, 2, interval, dtype=float)}
-                df = pd.DataFrame(data)
+                #constants
+                v_e = 267. 
+                P_a = 101325.
+                radius = 0.7
+                A_e = np.pi*(radius*0.001)**2
 
-                #Load in datasets
-                F_thrust = 'https://raw.githubusercontent.com/Roosevelt-Racers/Race-Time-Calculator-V2/main/data/F-thurst(v1).csv'
-                F_thrust = pd.read_csv(F_thrust)
-
-                Co2_mass = 'https://raw.githubusercontent.com/Roosevelt-Racers/Race-Time-Calculator-V2/main/data/Co2-mass(v1).csv'
-                Co2_mass = pd.read_csv(Co2_mass)
+                #kinematic equations
+                def car(y, t, c_d, A_f, m_0):
+                    mass, xdot, x = y
+                    ydot = [-(1/v_e)*(thrust(t)+A_e*(pressure(mass - car_mass + 0.008) - P_a)), 
+                            (1/mass)*(thrust(t) - drag(frontal_area, c_d, xdot) - friction(mass, friction_mu)), 
+                            xdot
+                            ]
+                    return ydot
                 
-                #Create one dataframe
-                df_loaded = pd.merge(F_thrust, Co2_mass, on='time(s)')
-                df_loaded = df_loaded.drop(columns="time(s)")
-                #df = pd.merge([df, df_loaded.reindex(df.index)])
-                df = pd.concat([df, df_loaded.reindex(df.index)], axis=1)
+                #intialize and generate solution
                 
-                #Create total mass as mass
-                Co2_empty = Co2_mass['Co2-mass(g)'].iloc[-1] + car_mass
-                df['Co2-mass(g)'] = Co2_mass['Co2-mass(g)'] + car_mass
-                df = df.rename(columns={"Co2-mass(g)": "mass(g)"})
+                y_0 = [car_mass, 0., 0.]
+                time = np.arange(0.,1.5,0.001)
 
-                #Fill Nan values
-                df['mass(g)'] = df['mass(g)'].fillna(Co2_empty)#[]p[][][]]
-                df['F-thrust(N)'] = df['F-thrust(N)'].fillna(0)
+                solution = odeint(car, y_0, time, args = (drag_coeff, frontal_area, car_mass))
+                
+                msol = solution[:, 0]
+                vsol = solution[:, 1]
+                xsol = solution[:, 2]
 
-                #Calculate Friction Force
-                df['F-friction(N)'] = [cal_friction_f(total_mass=row,friction_mu=friction_mu) for row in df['mass(g)']]
-                print(df)
-
-
-            ############################################################3
-            #Calculate DVA
-            
-                # #Create the Dataframe
-                df_dva = pd.DataFrame(columns=('time(s)', 'F-thrust(N)', 'mass(g)', 'F-friction(N)', 'F-drag(N)', 'F-net(N)', 'acceleration(m/s^2)', 'delta-v(m/s)', 'velocity(m/s)', 'delta-d(m)', 'distance(m)'))
-                df_dva.loc[0] = [df.loc[0, 'time(s)'], df.loc[0, 'F-thrust(N)'], df.loc[0, 'mass(g)'], df.loc[0, 'F-friction(N)'], 0, 0, 0, 0, 0, 0, 0]
-
-                index = 1
-                while df_dva['distance(m)'].values[-1] < 20:
-                    """While the distance is not greater than 20 contiue calculating"""
-
-                    #print("df_dva\n",df_dva)
-
-                    #Append the known values
-                    df_dva = pd.concat([df_dva,pd.DataFrame(
-                        {
-                        'time(s)': [df.loc[index, 'time(s)']],
-                        'F-thrust(N)': [df.loc[index, 'F-thrust(N)']],
-                        'mass(g)': [df.loc[index, 'mass(g)']],
-                        'F-friction(N)': [df.loc[index, 'F-friction(N)']], 
-                        }
-                        )], ignore_index=True)
-                    
-                    #Read the Drag based off the previous velocity
-                    """The problem is here the list is here right beneath the word here """
-                    #Get variable datavalues
-                    F_T = df_dva.loc[index, 'F-thrust(N)']
-                    F_f = df_dva.loc[index, 'F-friction(N)']
-                    F_d = df_dva.loc[index, 'F-drag(N)']
-                    m = df_dva.loc[index, 'mass(g)']
-                    v = df_dva.loc[index, 'velocity(m/s)']
-                    x = df_dva.loc[index, 'distance(m)']
-                    dt = interval
-                    
-                    #Calculations
-                    F_net = F_T - F_f - F_d
-                    a = F_net/m
-                    dv = a*dt
-                    dx = v*dx
-                    v += dv
-                    x += dx
-
-                    #reassign values
-                    
-
-
-
-                    df_dva.loc[index, 'F-drag(N)'] = cal_drag_f(area=area, drag_mu=drag_mu, velocity=df_dva.loc[index-1, 'velocity(m/s)'])
-
-                    #Calculate the Fnet
-                    df_dva.loc[index, 'F-net(N)'] = (df_dva.loc[index, 'F-thrust(N)'] - df_dva.loc[index, 'F-friction(N)'] - df_dva.loc[index, 'F-drag(N)'])
-
-                    #Calculate the acceleration
-                    df_dva.loc[index, 'acceleration(m/s^2)'] = (df_dva.loc[index, 'F-net(N)']/df_dva.loc[index, 'mass(g)'] * 1000)
-
-                    #Calculate the delta-velocity
-                    df_dva.loc[index, 'delta-v(m/s)'] = (interval * (df_dva.loc[index-1, 'acceleration(m/s^2)'] + df_dva.loc[index, 'acceleration(m/s^2)']) / 2)
-
-                    #Calculate the velocity
-                    df_dva.loc[index, 'velocity(m/s)'] = (df_dva.loc[index-1, 'velocity(m/s)'] + df_dva.loc[index, 'delta-v(m/s)'])
-
-                    #Calculate the delta-distance
-                    df_dva.loc[index, 'delta-d(m)'] = (interval * (df_dva.loc[index-1, 'velocity(m/s)'] + df_dva.loc[index, 'velocity(m/s)']) / 2)
-
-                    #Calculate the distance
-                    df_dva.loc[index, 'distance(m)'] = (df_dva.loc[index-1, 'distance(m)'] + df_dva.loc[index, 'delta-d(m)'])
-
-                    index += 1
+                #calculated values
+                Fsol = (thrust(time) - drag(frontal_area, drag_coeff, vsol) - friction(msol, friction_mu))
+                asol = Fsol/msol
 
             st.success('Done!')
 
 
         #---------------------------------------
-        # Metric
+        # Metrics
 
-            top_speed = (df_dva['velocity(m/s)'].max())*(18/5)
-            end_time = df_dva['time(s)'].values[-1]
+            top_speed = (vsol.max())
+
+            for i, x in enumerate(xsol):
+                end_time = time[i]
+                if x >= 20:
+                    break
 
             metric_col1, metric_col2 = st.columns(2)
-            metric_col1.metric("Top Speed (km/hr)", round(top_speed, 4))
-            metric_col2.metric("End time (sec)", round(end_time, 4))
+            metric_col1.metric("Top Speed (m/s)", round(top_speed, 4))
+            metric_col2.metric("Race time (sec)", round(end_time, 4))
             # metric_col3.metric("Efficiency", "86%", "4%")
 
         #---------------------------------------
         #Graphs
 
             #Acceleration Graph
-            acc_dataframe = df_dva[['time(s)', 'acceleration(m/s^2)']]
+            acc_dataframe = pd.Series(asol, index = time)
             st.header('Acceleration Over Time')
             acc_col1, acc_col2 = st.columns([3, 1])
             acc_col1.subheader('Acceleration Over Time Chart')
-            acc_col1.line_chart(acc_dataframe.rename(columns={'time(s)':'index'}).set_index('index'))
+            acc_col1.line_chart(acc_dataframe)
             acc_col2.subheader('Acceleration DataFrame')
             acc_col2.write(acc_dataframe)
+
             #Acceleration Expander
             acc_expander = st.expander('What did we do?')
             acc_expander.write("We did these calculation:")
-            acc_expander.latex(r'''F_{net} = F_{CO2} – F_{D} – F{f} = (m_{car} + m_{CO2}) a''')
+            acc_expander.latex(r'''F_{net} = F_{CO2} - F_{D} - F{f} = (m_{car} + m_{CO2}) a''')
 
             #Velocity Graph
-            v_dataframe = df_dva[['time(s)', 'velocity(m/s)']]
+            v_dataframe = vsol
             st.header('Velocity Over Time')
             v_col1, v_col2 = st.columns([3, 1])
             v_col1.subheader('Velocity Over Time Chart')
-            v_col1.line_chart(v_dataframe.rename(columns={'time(s)':'index'}).set_index('index'))
+            v_col1.line_chart(v_dataframe)
             v_col2.subheader('Velocity DataFrame')
             v_col2.write(v_dataframe)
+
             #Velocity Expander
             v_expander = st.expander('What did we do?')
             v_expander.write("We did these calculation:")
@@ -201,13 +178,14 @@ def app():
             ''')
             
             #Distance Graph
-            d_dataframe = df_dva[['time(s)', 'distance(m)']]
+            d_dataframe = xsol
             st.header('Distance Over Time')
             d_col1, d_col2 = st.columns([3, 1])
             d_col1.subheader('Distance Over Time Chart')
-            d_col1.line_chart(d_dataframe.rename(columns={'time(s)':'index'}).set_index('index'))
+            d_col1.line_chart(d_dataframe)
             d_col2.subheader('Distance DataFrame')
             d_col2.write(d_dataframe)
+
             #Distance Expander
             d_expander = st.expander('What did we do?')
             d_expander.write("We did these calculation:")
@@ -215,16 +193,14 @@ def app():
                 d_{n}=\sum_{0}^{n} \frac{\left[v\left(t_{n}\right)+v\left(t_{n+1}\right)\right]}{2} \bullet\left(t_{n+1}-t_{n}\right)
             ''')
 
-
             st.header("The full data table:")
-            st.write(df_dva)
+            st.write(solution)
 
-            dva_csv = df_dva.to_csv().encode('utf-8')
-            st.download_button(
-                label="Download DVA data as CSV",
-                data=dva_csv,
-                file_name='dva_data.csv',
-                mime='text/csv',)
+            # st.download_button(
+            #     label="Download DVA data as CSV",
+            #     data=np.array2string(solution),
+            #     file_name='data.csv',
+            #     mime='text/csv',)
 
 
 
